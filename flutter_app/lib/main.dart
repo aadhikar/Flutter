8b9cbae20056843f12814e1339bd3136f36adf99
Anilkumar Adhikari <aadhikar@users.noreply.github.com>
2019-01-20 02:54:52 -0600

    Formatting code
    Formatting code

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -5,13 +5,15 @@ import 'package:flutter_app/app_screens/basicListView.dart';
 import 'package:flutter_app/app_screens/listsView.dart';
 import 'package:flutter_app/app_screens/simpleInterestForm.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Exploring UI Widgets",
-      home: SimpleInterestForm(),
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.indigo,
-        accentColor: Colors.indigoAccent,
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Exploring UI Widgets",
+        home: SimpleInterestForm(),
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.indigo,
+          accentColor: Colors.indigoAccent,
+        ),
       ),
-    ));
+    );
